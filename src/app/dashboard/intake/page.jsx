3ca2a8ccd684f203bce0eb0b842ed0a9e2a8c10d
NoorAzam-1 <azamnoor698@gmail.com>
2026-06-10 17:19:09 +0530

    refactor(intake): use self-contained patient form

--- src/app/dashboard/intake/page.jsx
+++ src/app/dashboard/intake/page.jsx
@@ -1,34 +1,5 @@
-"use client";
-import { useRouter } from "next/navigation";
-import { useApp } from "@/context/AppContext";
-import { PatientIntakeForm } from "@/components/PatientIntakeForm";
+import { PatientIntakeForm } from "@/components/patientsForm/PatientIntakeForm";
 
 export default function Intake() {
-  const router = useRouter();
-  const { 
-    clinicalTemplateVer, storageStrategy, isDarkMode, 
-    patientIntakeForm, setPatientIntakeForm,
-    demographicFieldSettings, diagnosticModuleSettings, customClinicalFields,
-    savePatientRegistration, updateDemographicIntake, updateDiagnosticIntake, updateCustomParameterValue
-  } = useApp();
-
-  // Context mein save function ko router pass karke wrap kar rahe hain
-  const handleSave = (e) => savePatientRegistration(e, router);
-
-  return (
-    <PatientIntakeForm
-      clinicalTemplateVer={clinicalTemplateVer}
-      storageStrategy={storageStrategy}
-      isDarkMode={isDarkMode}
-      patientIntakeForm={patientIntakeForm}
-      setPatientIntakeForm={setPatientIntakeForm}
-      demographicFieldSettings={demographicFieldSettings}
-      diagnosticModuleSettings={diagnosticModuleSettings}
-      customClinicalFields={customClinicalFields}
-      savePatientRegistration={handleSave}
-      updateDemographicIntake={updateDemographicIntake}
-      updateDiagnosticIntake={updateDiagnosticIntake}
-      updateCustomParameterValue={updateCustomParameterValue}
-    />
-  );
+  return <PatientIntakeForm />;
 }
