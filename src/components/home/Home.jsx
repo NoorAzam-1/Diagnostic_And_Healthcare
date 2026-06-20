d2960ac8e3ee4e9dca2212ae6ccfceb4ab2a42a9
NoorAzam-1 <azamnoor698@gmail.com>
2026-06-20 13:41:08 +0530

    refactor(Home): simplify layout by removing unnecessary wrapper div

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -9,9 +9,7 @@ import DynamicLabConfigurationSection from "./DynamicLabConfigurationSection";
 
 export default function Home() {
   return (
-    <div className="min-h-screen bg-slate-950 text-slate-200 relative overflow-hidden">
-      <div className="absolute bottom-0 right-0 w-[500px] h-[500px] bg-violet-600/20 blur-[140px] rounded-full pointer-events-none" />
-
+    <>
       <Header />
       <HeroSection />
       <TrustedBySection />
@@ -20,6 +18,6 @@ export default function Home() {
       <RoleAccessSection />
       <DynamicLabConfigurationSection />
       <LandingFooter />
-    </div>
+    </>
   );
 }
