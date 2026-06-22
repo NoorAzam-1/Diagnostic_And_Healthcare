ccaf1f8ea423879faf1e2e0e3e88797a9a0708a0
NoorAzam-1 <azamnoor698@gmail.com>
2026-06-22 13:02:22 +0530

    feat(page.js): simplify component by removing commented code and unnecessary whitespace

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,4 @@
 import Home from "@/components/home/Home";
-
-
 export default function page() {
-    return <Home />;
-  }
-  
-  
-//   import Test from "@/components/Test";
-
-// export default function page() {
-//   return <Test />;
-// }
-
-
+  return <Home />;
+}
