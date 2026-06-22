aa85861b8c859cf620329cbe1c7bd93a22a662ae
NoorAzam-1 <azamnoor698@gmail.com>
2026-06-22 12:47:38 +0530

    feat(Protocols): simplify component by removing unused context and props

--- src/app/dashboard/protocols/page.jsx
+++ src/app/dashboard/protocols/page.jsx
@@ -1,31 +1,5 @@
-"use client";
-import { useApp } from "@/context/AppContext";
-import { ProtocolConfig } from "@/components/acess/ProtocolConfig";
+import ProtocolConfig from "@/components/acess/ProtocolConfig";
 
 export default function Protocols() {
-  const { 
-    isDarkMode, storageStrategy, isMigratingData, migrateStorageType, dataMigrationProgress,
-    demographicFieldSettings, setDemographicFieldSettings, diagnosticModuleSettings, setDiagnosticModuleSettings,
-    customClinicalFields, newFieldCreator, setNewFieldCreator, createCustomClinicalField, deleteCustomField, saveClinicalTemplate 
-  } = useApp();
-
-  return (
-    <ProtocolConfig
-      isDarkMode={isDarkMode}
-      storageStrategy={storageStrategy}
-      isMigratingData={isMigratingData}
-      migrateStorageType={migrateStorageType}
-      dataMigrationProgress={dataMigrationProgress}
-      demographicFieldSettings={demographicFieldSettings}
-      setDemographicFieldSettings={setDemographicFieldSettings}
-      diagnosticModuleSettings={diagnosticModuleSettings}
-      setDiagnosticModuleSettings={setDiagnosticModuleSettings}
-      customClinicalFields={customClinicalFields}
-      newFieldCreator={newFieldCreator}
-      setNewFieldCreator={setNewFieldCreator}
-      createCustomClinicalField={createCustomClinicalField}
-      deleteCustomField={deleteCustomField}
-      saveClinicalTemplate={saveClinicalTemplate}
-    />
-  );
+  return <ProtocolConfig />;
 }
