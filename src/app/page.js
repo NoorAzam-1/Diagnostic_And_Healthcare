86e3bbb80df546058b6356c37fd3c25db1a0d2b4
NoorAzam-1 <azamnoor698@gmail.com>
2026-06-29 12:29:15 +0530

    Compose homepage sections in app route

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,35 @@
-import Home from "@/components/home/Home";
+import CTASection from "@/components/home/CTASection";
+import DynamicLabConfiguration from "@/components/home/DynamicLabConfiguration";
+import FAQs from "@/components/home/FAQs";
+import HeroSection from "@/components/home/HeroSection";
+import HowItWorks from "@/components/home/HowItsWorks";
+import Platform from "@/components/home/Platform";
+import Pricing from "@/components/home/Pricing";
+import ProductOverview from "@/components/home/ProductOverview";
+import RoleAccessSection from "@/components/home/RoleAccessSection";
+import SecurityCompliance from "@/components/home/SecurityCompliance";
+import StorageArchitecture from "@/components/home/StorageArchitecture";
+import Testimonials from "@/components/home/Testimonials";
+import TrustedBySection from "@/components/home/TrustedBySection";
+import WhyChooseHaraiOne from "@/components/home/WhyChoose";
+
 export default function page() {
-  return <Home />;
+  return (
+    <>
+      <HeroSection />
+      <TrustedBySection />
+      <ProductOverview />
+      <HowItWorks />
+      <WhyChooseHaraiOne />
+      <Platform />
+      <RoleAccessSection />
+      <DynamicLabConfiguration />
+      <StorageArchitecture />
+      <SecurityCompliance />
+      <Testimonials />
+      <Pricing />
+      <FAQs />
+      <CTASection />
+    </>
+  );
 }
