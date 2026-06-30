77e50ff9653bf746d6912acf19394f568eada410
NoorAzam-1 <azamnoor698@gmail.com>
2026-06-30 17:35:40 +0530

    Refactor About page to include additional components and improve structure

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,9 +1,17 @@
 import AboutHero from "@/components/about/AboutHero";
+import Build from "@/components/about/Build";
+import Design from "@/components/about/Design";
+import Problem from "@/components/about/Problem";
+import WhyChoose from "@/components/about/WhyChoose";
 
-export default function page(){
-    return(
-        <>
-        <AboutHero />
-        </>
-    )
+export default function page() {
+  return (
+    <div className="bg-background">
+      <AboutHero />
+      <Problem />
+      <WhyChoose />
+      <Design />
+      {/* <Build /> */}
+    </div>
+  );
 }
